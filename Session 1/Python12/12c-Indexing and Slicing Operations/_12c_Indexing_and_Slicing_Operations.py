b417f061946aaa9562d0cd7395ca23c87405ed18
Shah Thittinan <thittinan.shah@gmail.com>
2024-02-06 17:25:16 +0100

    Python 11,12 topic codes

--- Session 1/Python12/12c-Indexing and Slicing Operations/_12c_Indexing_and_Slicing_Operations.py
+++ Session 1/Python12/12c-Indexing and Slicing Operations/_12c_Indexing_and_Slicing_Operations.py
@@ -6,3 +6,8 @@ employees = ["Alice", "Vera", "Flo", "Mel"]
 print("Region: ", regions[0], "\nSales: ", sales[0])
 # [-1] represents the last item in a list, [-2] second to last
 print("Region: ", regions[-1], "\nSales: ", sales[-1])
+
+employees[2] = "Belle" # Changing data in a list at index "Flo" to "Belle"
+
+for employee in employees:
+    print(employee)
